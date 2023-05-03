2321e242e2ef902f8af0308b93dd1789ca43f22c
231051TertiusJ <125471734+231051TertiusJ@users.noreply.github.com>
2023-05-03 19:59:00 +0200

    end

---
+++ js/index.js
@@ -0,0 +1,24 @@
+console.log("Hello World");
+
+var firstName = "Tertius";
+let surname = "Jonker";
+let age = 18;
+const subject = "Development";
+
+let message = "Hello " + firstName + " " + surname + ", You are  " +  age + " Years old, and your subject is " + subject;
+
+console.log(message)
+
+let tim = 12;
+let jane = 22;
+let john = 14;
+
+tim+= 10;
+
+let teamTotal = tim + jane;
+let victory = john / 2;
+let total = teamTotal + victory;
+
+let answer = "Tim and Jane now have " + total + " marbles!";
+
+console.log(answer)
